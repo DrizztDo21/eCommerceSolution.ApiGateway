5c636201178e8391c5c0e37677ae7febacce05c2
DrizztDo21 <daniel.moro.fernandez@gmail.com>
2025-09-18 09:57:39 +0200

    feat(polly): Add polly policies, rate limit and response caching
    Added basic implementation of global policies for all routes and specific policies for products
    Added rate limit for products
    Added response caching for orders

---
+++ ApiGateway/Policies/PollyHandler.cs
@@ -0,0 +1,30 @@
+﻿using Polly;
+using Polly.CircuitBreaker;
+using Polly.Retry;
+
+namespace ApiGateway.Policies
+{
+    public class PollyHandler : DelegatingHandler
+    {
+        private readonly AsyncRetryPolicy<HttpResponseMessage> _retryPolicy;
+        private readonly AsyncCircuitBreakerPolicy<HttpResponseMessage> _circuitBreakerPolicy;
+
+        public PollyHandler()
+        {
+            _retryPolicy = Policy
+                .HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+                .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(2));
+
+            _circuitBreakerPolicy = Policy
+                .HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+                .CircuitBreakerAsync(3, TimeSpan.FromSeconds(10));
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return await _retryPolicy.ExecuteAsync(() =>
+                   _circuitBreakerPolicy.ExecuteAsync(() =>
+                   base.SendAsync(request, cancellationToken)));
+        }
+    }
+}
--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,11 +1,20 @@
+using ApiGateway.Policies;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Provider.Polly;
+using Polly;
+using Polly.Extensions.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
-builder.Services.AddOcelot();
+builder.Services.AddOcelot(builder.Configuration).
+    AddPolly().
+    AddDelegatingHandler<PollyHandler>(true);
+
+
+
 
 
 var app = builder.Build();
